4bc05f8e269746f24c77542dd566b9572eab6191
trinityXmontoya <trinityXmontoya@gmail.com>
2014-10-03 11:02:20 -0400

    add math engine

---
+++ math_engine.rb
@@ -0,0 +1,11 @@
+# sinatra math engine
+# get and post accept params in the same way in sinatra so this answer applies for both.
+
+# require 'json'
+
+get '/nums' do
+  arr = params["values"]
+  sum = arr.inject(:+)
+  product = arr.inject(:*)
+  return { "sum": sum, "product":product }.to_json
+end
